46b33e5574ecd5a41057b36a904e19bbb8ce0da9
hyerim <hyerimiya1216@gmail.com>
2023-01-28 16:06:55 +0900

    Feat : 메뉴 수정 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,37 @@
 /*
-<메뉴추가 요구사항>
+TODO 메뉴 추가
 - 메뉴 이름을 입력 받고 확인버튼 OR 엔터키 로 메뉴가 추가된다.
 - 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 - 총 메뉴 갯수를 count하여 상단에 보여준다.
 - 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 - 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+TODO 메뉴 수정
+- 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴를 수정할 수 있는 모달창이 뜬다.(prompt 인터페이스)
+- 모달창(prompt)에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴이름이 수정된다.
 */
 
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+    // 부모요소에 이벤트 위임할 수 있다.
+    $('#espresso-menu-list').addEventListener('click', (e) => { 
+        // 수정버튼만 지정될 수 있도록 설정
+        if (e.target.classList.contains('menu-edit-button')) {
+            // target에서 가장 가까운 요소를 찾아(closest)
+            const $menuName = e.target.closest('li').querySelector('.menu-name');
+            // 기존 메뉴이름이 prompt창에 디폴트값으로 입력되어있음
+            const uqdateMenuName = prompt(
+                "메뉴명을 수정하세요", 
+                $menuName.innerText
+            );
+            // 변경한 메뉴명으로 업데이트 해준다.
+            $menuName.innerText = uqdateMenuName;
+
+            console.log(e.target);
+        }
+    })
+
     // form태그가 자동으로 전송되는 걸 막아준다
     $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
