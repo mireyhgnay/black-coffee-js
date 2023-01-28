44af2860474adfb42049f59ff7342c28cd5d3d5f
hyerim <hyerimiya1216@gmail.com>
2023-01-28 16:43:35 +0900

    Docs : 요구사항 step2 분석하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 /*
+step1 요구사항 - 돔 조작과 이벤트 핸들링으로 메뉴 관리하기
 TODO 메뉴 추가
 - 메뉴 이름을 입력 받고 확인버튼 OR 엔터키 로 메뉴가 추가된다.
 - 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
@@ -14,6 +15,30 @@ TODO 메뉴 삭제
 - 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.(confirm 인터페이스)
 - 확인 버튼을 클릭하면 메뉴가 삭제된다.
 - 총 메뉴 갯수를 count해서 상단에 보여준다.
+*/
+
+/*
+step2 요구사항 - 상태 관리로 메뉴 관리하기
+TODO localStorage Read & Write
+- localStorage에 데이터를 저장한다.    
+- localStorage에 있는 데이터를 읽어온다.
+
+TODO 카테고리별 메뉴판 관리
+- 에스프레소 메뉴판 관리
+- 프라푸치노 메뉴판 관리
+- 블렌디드 메뉴판 관리
+- 티바나 메뉴판 관리
+- 디저트 메뉴판 관리
+
+TODO 페이지 접근시 최초 데이터 Read & Rendering
+- 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
+- 에스프레소 메뉴를 페이지에 그려준다.
+
+TODO 품절 상태 관리
+- 품절 상태인 겨우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+- 품절 버튼을 추가한다.
+- 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+- 클릭이벤트에서 가장 가까운 li태그의 class속성 값에 sold-out을 추가한다.
 */
 
 const $ = (selector) => document.querySelector(selector);
